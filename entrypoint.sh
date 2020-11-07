fab7e8689e7b328703bfa213967262cd9afddd97
tvrcgo <tvrcgo@gmail.com>
2020-11-08 03:19:39 +0800

    output by date

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,6 @@
 sh /lede/scripts/build.sh
 
 # move firmware
-mv /lede/src/bin/targets/* /lede/firmware/
+FIRMWARE_DIR=/lede/firmware/$(date +"%Y%m%d%H%M")
+mkdir -p $FIRMWARE_DIR
+mv /lede/src/bin/targets/* $FIRMWARE_DIR
