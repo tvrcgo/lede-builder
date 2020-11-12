474edc670ad35e21051cb948cec70836e300d1cc
tvrcgo <tvrcgo@gmail.com>
2020-11-12 16:40:56 +0800

    target

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # prepare
-CONFIG_FILE=/openwrt/config/$DEVICE_NAME.config
+CONFIG_FILE=/openwrt/config/$TARGET.config
 
 # build firmware
 sh /openwrt/scripts/build.sh
 
 # move firmware
-FIRMWARE_DIR=/openwrt/firmware/$DEVICE_NAME-$(date +"%Y%m%d%H%M")
+FIRMWARE_DIR=/openwrt/firmware/$TARGET-$(date +"%Y%m%d%H%M")
 mkdir -p $FIRMWARE_DIR
 mv /openwrt/src/bin/targets/* $FIRMWARE_DIR
