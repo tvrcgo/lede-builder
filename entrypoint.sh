4eadd78eee1e4805bce01bc42bf5f021be342aa8
tvrcgo <tvrcgo@gmail.com>
2020-11-12 20:33:47 +0800

    fix mkdir

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,5 +8,5 @@ sh /openwrt/scripts/build.sh
 
 # move firmware
 FIRMWARE_DIR=/openwrt/firmware/$TARGET-$(date +"%Y%m%d%H%M")
-mkdir -p $FIRMWARE_DIR
+mkdir -p "$FIRMWARE_DIR"
 mv -f /openwrt/src/bin/targets/* $FIRMWARE_DIR
